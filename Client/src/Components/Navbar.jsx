475798c7a46df9fa37ee8b984fc220f6ecfcaec8
kavithachinnusamy20 <kavithachinnusamy20@gmail.com>
2025-07-15 22:09:49 -0400

    adding style for Navbar

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css"
 
 const Navbar =()=>{
     return (
     <nav> 
-        <h1>Navy</h1>
+        <h1>Welcome to Brainy Bites Quiz App</h1>
         <ul>
             <Link to ="/">Home</Link>
             <Link to ="/About">About</Link>
