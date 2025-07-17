61aea2295f241d1715ac976725df3c3a8c0512c9
kavithachinnusamy20 <kavithachinnusamy20@gmail.com>
2025-07-17 10:41:25 -0400

    questions to render radio buttons

--- Client/src/Components/questions.jsx
+++ Client/src/Components/questions.jsx
@@ -1,30 +1,48 @@
  import React from 'react'
-import { useState } from 'react'
+ import { useState ,useEffect} from 'react'
+
+ import data from '../Database/data' // Imports the quiz question data from your local file
  
-
-  
+ 
+ 
  const Questions=() =>{
-
+    
     const [checked,setChecked]= useState(undefined)
+    
+    const question =data[0]
+    
+    useEffect(() => {
+        console.log(question)
+    })
+    
     function onSelect(){
-        console.log('radio button cheange')
+        console.log('radio button change')
     }
     return(
         <div className= 'questions'>
-        <h2 className='text-light'>Simple Question 1</h2>
-        <ul>
-            <li>
+        {/* Displays the question text.    */}
+        <h2 className='text-light'>{question.question}</h2>
+        
+        <ul key= {question.id}>
+        {
+            question.options.map((q,i)=>(
+                <li key={i}>
                 <input 
                 type ="radio" 
                 value ={true}
-                id ='q1-option'
-                 onChange ={onSelect()}
-                 />
-                 <label className='text-primary' htmlFor="q1-option">option</label>
-                 <div className ='check checked'></div>
-            </li>
+                id ={`q${i}-option`}
+                onChange ={onSelect()}
+                />
+                <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
+                <div className ='check checked'></div>
+                </li>
+            ))
+        }
+        
+        
+        
         </ul>
         </div>
     )
- }
- export default Questions
+}
+export default Questions
