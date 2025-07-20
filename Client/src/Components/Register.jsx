996fa694fddb74c1589585a195c719dcc9a0b3ad
kavithachinnusamy20 <kavithachinnusamy20@gmail.com>
2025-07-20 18:10:26 -0400

    Invoking user validations in the form

--- Client/src/Components/Register.jsx
+++ Client/src/Components/Register.jsx
@@ -1,76 +1,87 @@
- import { useState } from 'react';
-import React from 'react'
-  import Navbar from './Navbar'
-  import axios from "axios";
-import {Link} from "react-router-dom"
+import React from 'react';
 
-  
- const Register =() =>{
+import useForm from '../hooks/useForm';
 
-     const [name, setName] = useState()
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-
-     const handleSubmit =(e)=>{
-        e.preventDefault()
-        axios.post('http://localhost:8080/register',{name,email,password})
-        .then(result => console.log(result))
-        .catch(err=> console.log(err))
-    }
-
+const Register = () => {
+    
+    const { handleChange, values, handleSubmit,errors } = useForm();
     
     return (
-        <div className="signup-container">
-        <div className="signup-box">
-        <h2>Register</h2>
-        <form onSubmit={handleSubmit}>
-        <div className="form-group">
-        <label htmlFor="name"><strong>Name</strong></label>
-        <input
-        type="text"
-        placeholder="Enter Name"
-        autoComplete="off"
-        name="name"
-        className="form-input"
-        onChange ={(e)=>setName(e.target.value)}
-        />
+        <div className ="form-content-right">
+            <h1>Create your Account</h1>
+            <form className="form" onSubmit={handleSubmit} >
+                    <div className="form-inputs">
+                        <label htmlFor="username" className='form-label'>
+                        Username
+                        </label>
+                        <input 
+                            type="text"
+                            id ="username"
+                            name ="username"
+                            className ="form-input"
+                            placeholder="Enter your username"
+                            value={values.username}
+                            onChange={handleChange}
+                        />
+                        {errors.username && <p>{errors.username}</p>}
+                </div>
+            
+                <div className="form-inputs">
+                <label htmlFor="email" className='form-label'>
+                    Email
+                    </label>
+                <input 
+                    type="text"
+                    id="email"
+                    name ="email"
+                    className ="fprm-input"
+                    placeholder="Enter your email"
+                    value ={values.email}
+                    onChange={handleChange}
+                />
+                   {errors.email && <p>{errors.email}</p>}
+                </div>
+                
+                <div className="form-inputs">
+                <label htmlFor="password" className='form-label'>
+                password
+                </label>
+                <input 
+                    type="password"
+                    id="password"
+                    name ="password"
+                    className ="form-input"
+                    placeholder="Enter your password"
+                    value ={values.password}
+                    onChange={handleChange}
+                />
+                   {errors.password && <p>{errors.password}</p>}
+                </div>
+                
+                
+                <div className="form-inputs">
+                <label htmlFor="password2" className='form-label'>
+                Confirm password
+                </ label>
+                <input 
+                    type="password"
+                    id="password2"
+                    name ="password2"
+                    className ="form-input"
+                    placeholder="Enter your confirm password"
+                    value ={values.password2}
+                    onChange={handleChange}
+                />
+                    {errors.password2 && <p>{errors.password2}</p>}
+                </div>
+            
+                <button className ="form-input btn" type ="submit">Signup </button>
+                <span className="form-input-login">
+                    Already have an account?Login<a href='#'>here</a>
+                </span>
+            </form>
         </div>
-        
-        <div className="form-group">
-        <label htmlFor="email"><strong>Email</strong></label>
-        <input
-        type="email"
-        placeholder="Enter Email"
-        autoComplete="off"
-        name="email"
-        className="form-input"
-        onChange ={(e)=>setEmail(e.target.value)}
-        />
-        </div>
-        
-        <div className="form-group">
-        <label htmlFor="password"><strong>Password</strong></label>
-        <input
-        type="password"
-        placeholder="Enter Password"
-        autoComplete="off"
-        name="password"
-        className="form-input"
-        onChange ={(e)=>setPassword(e.target.value)}
-        />
-        </div>
-        
-        <button type="submit" className="form-button">
-        Register
-        </button>
-        
-        <p className="login-text">Already Have an Account?</p>
-        <Link to="/login" type="button" className="login-button">
-        Login
-        </Link>
-        </form>
-        </div>
-        </div>
-    );
- }
- export default Register
+    )
+}
+
+export default Register;
